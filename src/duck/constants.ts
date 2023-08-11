c7fca61f5ec3b353290a6ac4e1f01f4cee148a86
dpnguyen53 <dpnguyen53@gmail.com>
2023-08-11 20:37:41 +0700

    upload

---
+++ src/duck/constants.ts
@@ -0,0 +1,6 @@
+export const FETCH_LIST_PRODUCT_REQUEST =
+  "@@productReducer/FETCH_LIST_PRODUCT_REQUEST";
+export const FETCH_LIST_PRODUCT_SUCCESS =
+  "@@productReducer/FETCH_LIST_PRODUCT_SUCCESS";
+export const FETCH_LIST_PRODUCT_FAIL =
+  "@@productReducer/FETCH_LIST_PRODUCT_FAIL";
